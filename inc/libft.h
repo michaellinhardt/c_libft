5e2538e0ae33603492d13d9ab25e1142700b044a
Michael Linhardt <nestoyeur@gmail.com>
2015-11-27 00:31:14 +0100

    push 8 new functions

--- inc/libft.h
+++ inc/libft.h
@@ -5,8 +5,19 @@
 # include <unistd.h>
 # include <string.h>
 
-void	ft_putchar(char c);
 void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char const *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
+
+void	ft_putchar(char c);
+void	ft_putstr(char const *s);
+void	ft_putnbr(int n);
+
 size_t	ft_strlen(const char *s);
+
+int		ft_tolower(int c);
+int		ft_toupper(int c);
+int		ft_islower(char c);
+int		ft_isupper(char c);
 
 #endif
---
+++ src/ft_putnbr_fd.c
@@ -0,0 +1,10 @@
+#include "libft.h"
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	if (n < 0 && (n = -n))
+		ft_putchar_fd('-', fd);
+	if ((n / 10) > 0)
+		ft_putnbr_fd(n / 10, fd);
+	ft_putchar_fd((n % 10) + '0', fd);
+}
