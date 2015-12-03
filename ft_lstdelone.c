2bfac217d2c5647f1313757a41824751aab038b7
Michael LINHARDT <mlinhard@e1r12p1.42.fr>
2015-12-03 20:25:07 +0100

    lstdelone NO TEST

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,8 +6,17 @@
 /*   By: mlinhard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/03 20:05:43 by mlinhard          #+#    #+#             */
-/*   Updated: 2015/12/03 20:05:52 by mlinhard         ###   ########.fr       */
+/*   Updated: 2015/12/03 20:24:55 by mlinhard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+void	ft_lstdelone(t_list **alst, void (*del)(void *, size_t))
+{
+	if (!alst || !del)
+		return ;
+	del((*alst)->content, (*alst)->content_size);
+	free(*alst);
+	*alst = NULL;
+}
