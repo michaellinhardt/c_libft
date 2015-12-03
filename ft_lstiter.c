74c2d7a4e90237704b102f80ff398b4de85667e1
Michael LINHARDT <mlinhard@e1r12p1.42.fr>
2015-12-03 20:27:35 +0100

    lstiter NO TEST

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -6,8 +6,23 @@
 /*   By: mlinhard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/03 20:05:58 by mlinhard          #+#    #+#             */
-/*   Updated: 2015/12/03 20:06:07 by mlinhard         ###   ########.fr       */
+/*   Updated: 2015/12/03 20:27:12 by mlinhard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+void	ft_lstiter(t_list *lst, void (f)(t_list *elem))
+{
+	t_list *list;
+
+	if (lst)
+	{
+		list = lst;
+		while (list)
+		{
+			f(list);
+			list = list->next;
+		}
+	}
+}
