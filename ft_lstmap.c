108ea05e8b91ee729d20478fd74800ba26e328c9
Michael LINHARDT <mlinhard@e1r12p1.42.fr>
2015-12-03 20:30:26 +0100

    lstmap REALLY NEED TEST

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,8 +6,32 @@
 /*   By: mlinhard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/03 20:06:11 by mlinhard          #+#    #+#             */
-/*   Updated: 2015/12/03 20:06:20 by mlinhard         ###   ########.fr       */
+/*   Updated: 2015/12/03 20:30:02 by mlinhard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+t_list		*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
+{
+	t_list *new_lst;
+	t_list *new_elem;
+	t_list *prev_elem;
+
+	new_lst = NULL;
+	if (lst && (*f))
+	{
+		new_lst = (*f)(lst);
+		prev_elem = new_lst;
+		lst = lst->next;
+		while (lst)
+		{
+			new_elem = (*f)(lst);
+			prev_elem->next = new_elem;
+			prev_elem = new_elem;
+			lst = lst->next;
+		}
+		prev_elem->next = NULL;
+	}
+	return (new_lst);
+}
