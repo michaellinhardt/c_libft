91e227ddcddfa24ba4597a2f2e582190c871c9ea
Michael LINHARDT <mlinhard@e1r12p1.42.fr>
2015-12-03 20:23:41 +0100

    lstdel NO TEST

--- ft_lstdel.c
+++ ft_lstdel.c
@@ -6,8 +6,24 @@
 /*   By: mlinhard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/03 20:05:31 by mlinhard          #+#    #+#             */
-/*   Updated: 2015/12/03 20:05:39 by mlinhard         ###   ########.fr       */
+/*   Updated: 2015/12/03 20:23:21 by mlinhard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
+{
+	t_list	*list;
+
+	if (!alst || !del)
+		return ;
+	list = *alst;
+	while (list)
+	{
+		del(list->content, list->content_size);
+		free(list);
+		list = list->next;
+	}
+	*alst = NULL;
+}
