476932726aeecb14595bdbb478ba55c8be2307cc
Michael LINHARDT <mlinhard@e1r12p1.42.fr>
2015-12-03 20:19:25 +0100

    lstadd NO TEST

--- ft_lstadd.c
+++ ft_lstadd.c
@@ -6,8 +6,14 @@
 /*   By: mlinhard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/03 20:05:14 by mlinhard          #+#    #+#             */
-/*   Updated: 2015/12/03 20:05:26 by mlinhard         ###   ########.fr       */
+/*   Updated: 2015/12/03 20:19:03 by mlinhard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+void	ft_lstadd(t_list **alst, t_list *new)
+{
+	new->next = *alst;
+	*alst = new;
+}
