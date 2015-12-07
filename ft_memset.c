c4fcbbc64082bd164150e45aacf98ace7d36195f
Michael LINHARDT <mlinhard@e1r13p1.42.fr>
2015-12-08 00:42:42 +0100

    unprotect

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: mlinhard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/28 13:42:14 by mlinhard          #+#    #+#             */
-/*   Updated: 2015/11/28 13:42:16 by mlinhard         ###   ########.fr       */
+/*   Updated: 2015/12/07 23:51:55 by mlinhard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,8 @@ void	*ft_memset(void *s, int c, size_t n)
 	if (n == 0)
 		return (s);
 	byte = (char *)s;
-	if (!byte)
-		return (s);
+//	if (!byte)
+//		return (s);
 	while (n--)
 		*byte++ = (char)c;
 	return (s);
