0d15a45b2b8bcdba0be148e54d018894b4dd318b
Michael LINHARDT <mlinhard@e1r7p22.42.fr>
2015-12-10 14:39:30 +0100

    now use ft_memdel

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: mlinhard <mlinhard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/03 20:05:43 by mlinhard          #+#    #+#             */
-/*   Updated: 2015/12/08 03:35:29 by mlinhard         ###   ########.fr       */
+/*   Updated: 2015/12/10 09:09:17 by mlinhard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,5 @@ void	ft_lstdelone(t_list **alst, void (*del)(void *, size_t))
 	if (!alst || !del)
 		return ;
 	del((*alst)->content, (*alst)->content_size);
-	free(*alst);
-	*alst = NULL;
+	ft_memdel((void **)alst);
 }
