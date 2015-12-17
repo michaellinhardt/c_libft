72afa9e6807825b84c2c667f56b386fd8d995af6
Michael LINHARDT <mlinhard@student.42.fr>
2015-12-18 00:47:57 +0100

    better implementation

--- ft_strrev.c
+++ ft_strrev.c
@@ -6,7 +6,7 @@
 /*   By: mlinhard <mlinhard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/11 10:53:19 by mlinhard          #+#    #+#             */
-/*   Updated: 2015/12/17 21:53:46 by mlinhard         ###   ########.fr       */
+/*   Updated: 2015/12/18 00:45:57 by mlinhard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,19 @@
 
 char	*ft_strrev(char *str)
 {
-	char	*str_end;
 	char	c;
 	int		i;
 	int		j;
 
 	if (!str)
 		return (NULL);
-	str_end = str;
 	i = 0;
 	j = ft_strlen(str) - 1;
 	while (i < j)
 	{
 		c = str[i];
-		str[i++] = str_end[j];
-		str_end[j--] = c;
+		str[i++] = str[j];
+		str[j--] = c;
 	}
 	return (str);
 }
