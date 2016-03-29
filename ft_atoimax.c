d4000d4070f5e9cc4d5a8d31744e4b57dfebc28f
Michael LINHARDT <nestoyeur@gmail.com>
2016-03-29 06:58:51 +0200

    add ft_atoimax

--- ft_atoimax.c
+++ ft_atoimax.c
@@ -6,16 +6,16 @@
 /*   By: mlinhard <mlinhard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/03 14:21:58 by mlinhard          #+#    #+#             */
-/*   Updated: 2016/03/29 06:56:36 by mlinhard         ###   ########.fr       */
+/*   Updated: 2015/12/16 19:03:57 by mlinhard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		ft_atoi(const char *str)
+intmax_t	ft_atoimax(const char *str)
 {
-	int		ret;
-	int		sign;
+	intmax_t	ret;
+	int			sign;
 
 	sign = 1;
 	ret = 0;
